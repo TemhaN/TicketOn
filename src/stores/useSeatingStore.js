e94a26424ed04000df48706c7992eeecea08659a
TemhaN <temhan2005s@gmail.com>
2024-05-16 11:04:41 +0500

    add seating

--- src/stores/useSeatingStore.js
+++ src/stores/useSeatingStore.js
@@ -7,6 +7,8 @@ export const useSeatingStore = defineStore('seating', () => {
 	const selectedShow = ref(null);
 	const concertsShow = useConcertsStore();
 	const rows = ref([]);
+	const selectedSeats = ref([]);
+	const token = ref(null);
 
 	const setSelectedShow = id =>
 		(selectedShow.value = concertsShow.findShowById(id));
@@ -19,10 +21,31 @@ export const useSeatingStore = defineStore('seating', () => {
 		console.log(rows.value);
 	}
 
+	async function fetchReservation() {
+		const res = await api.post(
+			`/concerts/${selectedShow.value.concertId}/shows/${selectedShow.value.id}/seating/reservation`
+		);
+	}
+
+	function selectSeat(row, seat) {
+		const index = selectedSeats.value.findIndex(
+			item => item.row == row && item.seat == seat
+		);
+
+		if (index == -1) {
+			selectedSeats.value.push({ row, seat });
+		} else {
+			selectedSeats.value.splice(index, 1);
+		}
+	}
+
 	return {
+		fetchReservation,
 		setSelectedShow,
+		selectedSeats,
 		selectedShow,
 		fetchSeating,
+		selectSeat,
 		rows,
 	};
 });
--- src/views/SeatingView.vue
+++ src/views/SeatingView.vue
@@ -2,12 +2,30 @@
 import CardComponent from '@/components/CardComponent.vue';
 import { useSeatingStore } from '@/stores/useSeatingStore';
 import { useRoute } from 'vue-router';
+// import func from '../../vue-temp/vue-editor-bridge';
 
 const seatingStore = useSeatingStore();
 const route = useRoute();
 
 seatingStore.setSelectedShow(route.params.id);
 seatingStore.fetchSeating();
+
+function selectSeat(event) {
+	const target = event.target;
+
+	if (
+		!target.dataset.seat ||
+		target.classList.contains('hall-row__seat--reserved')
+	) {
+		return;
+	}
+	const seat = target.dataset.seat;
+	const row = target.closest('div[data-row]').dataset.row;
+
+	target.classList.toggle('hall-row__seat--selected');
+
+	seatingStore.selectSeat(row, seat);
+}
 </script>
 
 <template>
@@ -26,8 +44,13 @@ seatingStore.fetchSeating();
 	<div class="row mt-5">
 		<div class="col-md-8 seating">
 			<div class="stage">Stage</div>
-			<div class="hall m-3">
-				<div v-for="row in seatingStore.rows" :key="row.id" class="hall-row">
+			<div class="hall m-3" @click="selectSeat">
+				<div
+					v-for="row in seatingStore.rows"
+					:key="row.id"
+					:data-row="row.id"
+					class="hall-row"
+				>
 					<div class="hall-row__name">{{ row.name }}</div>
 					<div class="hall-row__seats">
 						<div
@@ -49,8 +72,15 @@ seatingStore.fetchSeating();
 			<h4 class="text-center mt-5">Selected Seats</h4>
 			<div class="main-selected-seats text-center mt-5">
 				<div class="selected-group">
-					<p>Row: Stall 03, Seat: 6</p>
-					<p>Row: Stall 03, Seat: 7</p>
+					<p
+						v-for="place in seatingStore.selectedSeats"
+						:key="`${place.row}` + place.seat"
+					>
+						Row:
+						{{ seatingStore.rows.find(items => items.id == place.row).name }},
+						Seat:
+						{{ place.seat }}
+					</p>
 				</div>
 				<p class="mt-5">You seats expire in 02:35</p>
 				<button class="button button-booking-details m-5" type="submit">
